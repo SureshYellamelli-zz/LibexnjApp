8f679e4712495b785eb9271fefa42635d78330ce
ysuresh81@gmail.com <s.satya.yellamelli@accenture.com>
2017-07-23 08:10:42 +0530

    Signed-off-by: ysuresh81@gmail.com

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
+var port = process.env.port || 5000;
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
-
-app.get('/books',function(req,res){
-	res.send("Hello books !!! Welcome");
+app.get('/books', function (req, res) {
+	res.send('Hello books !!! Welcome');
 });
 
-app.listen(5000,function(){	
-	console.log('Example app listening on port 5000');
+app.listen(port, function () {
+	console.log('Example app listening on port '+port);
 });
